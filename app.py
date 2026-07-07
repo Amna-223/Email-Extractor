3f146c588997855b66e73731d3d67665904f1a77
Amna Saeed <aamnahsaeed223@gmail.com>
2026-07-07 19:35:57 +0500

    Make requirement.txt and Procfile for deployment

--- app.py
+++ app.py
@@ -121,4 +121,5 @@ def terms():
 
 if __name__ == '__main__':
     multiprocessing.freeze_support()
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=False)
